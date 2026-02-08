e5c8595dd7e63ae23168c5a5104cb6e4f5930f1e
Florian Witteler <github@flwi.de>
2026-02-08 13:55:56 +0100

    picking a random color and printing its value as hex

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,10 +7,16 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use core::fmt::UpperHex;
+
 use defmt::info;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
+use esp_hal::rmt::Rmt;
+use esp_hal::rng::Rng;
+use esp_hal::time::{Duration, Instant, Rate};
+use esp_hal_smartled::{SmartLedsAdapter, smart_led_buffer};
+use smart_leds::{RGB8, SmartLedsWrite as _};
 use {esp_backtrace as _, esp_println as _};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
@@ -26,13 +32,39 @@ fn main() -> ! {
     // generator version: 1.2.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+    let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
 
+    // led_buffer needs to be mutable - docs are wrong
+    let mut led_buffer = smart_led_buffer!(1);
+    let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, &mut led_buffer);
+
+    const LEVEL: u8 = 10;
+
+    // start with all leds off
+    let mut color = RGB8::default();
+
+    let rng = Rng::new();
+    let mut rng_buffer = [0u8; 16];
     loop {
-        info!("Hello world!");
+        // generate 3 u8 values at once
+        rng.read(&mut rng_buffer);
+
+        led.write([color].into_iter()).unwrap();
+
+        info!(
+            "Blink in color #{:02X}{:02X}{:02X}",
+            color.r, color.g, color.b
+        );
+
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
-    }
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
+        // set all primary colors at one - YES, rust can assign multiple props of a struct like this
+        (color.r, color.b, color.g) = (
+            (rng_buffer[0] as f32 * (LEVEL as f32 / u8::max_value() as f32)) as u8,
+            (rng_buffer[1] as f32 * (LEVEL as f32 / u8::max_value() as f32)) as u8,
+            (rng_buffer[2] as f32 * (LEVEL as f32 / u8::max_value() as f32)) as u8,
+        )
+    }
 }
